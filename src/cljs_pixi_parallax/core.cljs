604847c051992de4fb2f242060943846a2c53e4b
Justin May <may.justin@gmail.com>
2016-01-30 14:10:29 -0800

    Init pipeline setup and app-state populated
    No longer defining everything as a global, storing things in the
    app-state instead.

--- src/cljs_pixi_parallax/core.cljs
+++ src/cljs_pixi_parallax/core.cljs
@@ -1,52 +1,36 @@
 (ns cljs-pixi-parallax.core
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [cljsjs.pixi]
+            [goog.dom :as dom]
             [cljs.core.async :refer [<!] :as async]))
 
 (enable-console-print!)
 
-(def game-canvas (.getElementById js/document "game-canvas"))
-(def canvas-width (.-width game-canvas))
-(def canvas-height (.-height game-canvas))
-
-(defonce container (js/PIXI.Container.))
-
-(defonce renderer
-         (.autoDetectRenderer js/PIXI
-                              canvas-width
-                              canvas-height
-                              #js {:view game-canvas}))
+(def canvas-id "game-canvas")
 
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom {:text "Hello world!"}))
 
+(defn update-sprite-positions
+  [state]
+  (-> state (update-in [:scroller :bg-far-tiling-x] - 0.128)
+            (update-in [:scroller :bg-mid-tiling-x] - 0.64)))
+
 (defn new-renderer []
   (let [count (:__figwheel_counter @app-state)]
     (fn this []
-      (let [{:keys [sprites __figwheel_counter]} @app-state
-            far-x (aget (:bg-far sprites) "tilePosition" "x")
-            mid-x (aget (:bg-mid sprites) "tilePosition" "x")]
+      (let [{:keys [sprites renderer container scroller __figwheel_counter]}
+            (swap! app-state update-sprite-positions)]
         (when (= __figwheel_counter count)
           (js/requestAnimationFrame this)
           (aset (:bg-far sprites) "tilePosition" "x"
-                (- far-x 0.128))
+                (:bg-far-tiling-x scroller))
           (aset (:bg-mid sprites) "tilePosition" "x"
-                (- mid-x 0.64))
+                (:bg-mid-tiling-x scroller))
+          (aset (:bg-mid sprites) "y"
+                (:bg-mid-y scroller))
           (.render renderer container))))))
-
-(defn completed-loading-resources!
-  [resources]
-  (println "Resources loaded!")
-  (let [bg-far (js/PIXI.extras.TilingSprite.
-                 (aget resources "bg-far" "texture") 544 320)
-        bg-mid (js/PIXI.extras.TilingSprite.
-                 (aget resources "bg-mid" "texture") 544 208)]
-    (.addChild container bg-far)
-    (.addChild container bg-mid)
-    (set! (.-y bg-mid) 112)
-    (swap! app-state assoc-in [:sprites] {:bg-far bg-far
-                                          :bg-mid bg-mid})))
 
 (defn load-resources!
   [c]
@@ -57,12 +41,46 @@
                (async/put! c resources)
                (async/close! c)))))
 
+(defn add-sprites-to-container! []
+  (let [{:keys [container sprites]} @app-state]
+    (-> container (.addChild (:bg-far sprites))
+                  (.addChild (:bg-mid sprites)))))
+
+(defn init-scroller
+  [state]
+  (assoc state
+    :scroller {:bg-far-y 0
+               :bg-far-tiling-x 0
+               :bg-mid-y 112
+               :bg-mid-tiling-x 0}))
+
+(defn init-sprites
+  [state resources]
+  (merge state
+         {:sprites {:bg-far (js/PIXI.extras.TilingSprite.
+                              (aget resources "bg-far" "texture") 544 320)
+                    :bg-mid (js/PIXI.extras.TilingSprite.
+                              (aget resources "bg-mid" "texture") 544 208)}}))
+
+(defn init-pixi
+  [state]
+  (let [canvas (dom/getElement canvas-id)
+        width (.getAttribute canvas "width")
+        height (.getAttribute canvas "height")]
+    (assoc state
+      :container (js/PIXI.Container.)
+      :renderer (.autoDetectRenderer js/PIXI width height #js {:view canvas}))))
+
 (defonce boot!
   (go (let [c (async/chan)]
         (load-resources! c)
         (let [resources (<! c)]
-          (completed-loading-resources! resources)
-          ((new-renderer))))))
+          (swap! app-state
+                 #(-> % init-pixi
+                        (init-sprites  resources)
+                        init-scroller)))
+        (add-sprites-to-container!)
+        ((new-renderer)))))
 
 (defn on-js-reload []
   (swap! app-state update-in [:__figwheel_counter] inc)
